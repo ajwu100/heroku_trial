013bd92bf2bfd3ea53e4c8923cf6f10d6de42721
ajwu100 <ajwu100@gmail.com>
2020-12-21 22:44:20 -0500

    UPdated app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get(
+    "DATABASE_URL", "sqlite:///heroku_trial.sqlite")
+
+db = SQLAlchemy(app)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String)
 
 
 @app.route('/')
@@ -10,3 +21,18 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+@app.route('/api/tasks-postgres')
+def taskPostGres():
+    tasks = db.session.query(Task)
+    data = []
+
+    for task in tasks:
+        item = {
+            'id': task.id,
+            'description': task.description
+        }
+        data.append(item)
+
+    return jsonify(data)
